6175a0e59b94c9955c9760b3fe596f88b154dd61
Luis Federico Vilar Velazquez <luis_velazquez@estudante.sesisenai.org.br>
2023-03-15 16:38:50 -0300

    avances pequeños

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,42 +1,53 @@
 import GrupoInput from "./GrupoInput";
 
-function Formulario() {
+function Formulario(props) {
+
   return (
-    <form className="form container">
-      <div className="row">
-        <GrupoInput
-          textProp="Data"
-          classNameProp="form-group"
-          htmlForProps="data"
-          typeProp="date"
-          minProp="1"
-          stepProp="1"
-          idProp="data"
-          clasNameProp="form-control"
-          valueProp="1"
-        />
-        <GrupoInput
-          textProp="Quantidade"
-          classNameProp="form-group"
-          htmlForProps="quantidade"
-          typeProp="number"
-          minProp="1"
-          stepProp="1"
-          idProp="quantidade"
-          clasNameProp="form-control"
-          valueProp="1"
-        />
-        <GrupoInput
-          textProp="Valor"
-          classNameProp="form-group"
-          htmlForProps="quantidade"
-          typeProp="number"
-          minProp="1"
-          stepProp="1"
-          idProp="quantidade"
-          clasNameProp="form-control"
-          valueProp="1"
-        />
+    <form className="form  container form">
+      <div className="row justify-content-around">
+        <div className="form-group col">
+          <GrupoInput
+            textProp="Data"
+            classNameLabelProp="form-label"
+            classNameProp="form-group"
+            htmlForProps="data"
+            typeProp="date"
+            minProp={1}
+            stepProp={1}
+            idProp="data"
+            classNameInputProp="form-control"
+            valueProp={1}
+          />
+        </div>
+        <div className="form-group col">
+          <GrupoInput
+            textProp="Quantidade"
+            classNameLabelProp="form-label"
+            classNameProp="form-group"
+            htmlForProps="quantidade"
+            typeProp="number"
+            minProp={1}
+            stepProp={1}
+            idProp="quantidade"
+            classNameInputProp="form-control"
+            valueProp={1}
+          />
+        </div>
+
+        <div className="form-group col">
+          <GrupoInput
+            textProp="Valor"
+            classNameLabelProp="form-label"
+            classNameProp="form-group"
+            htmlForProps="valor"
+            typeProp="number"
+            minProp={1}
+            stepProp={1}
+            idProp="valor"
+            classNameInputProp="form-control"
+            valueProp={1}
+          />
+        </div>
 
         <div className="col-1 d-flex align-items-end justify-content-end">
           <button className="btn btn-primary" type="button">
@@ -47,5 +58,7 @@ function Formulario() {
     </form>
   );
 }
+
+
 
 export default Formulario;
--- src/components/GrupoInput.jsx
+++ src/components/GrupoInput.jsx
@@ -1,18 +1,32 @@
+import PropTypes from "prop-types";
+
 function GrupoInput(props) {
   return (
     <div className={props.classNameProp}>
-      <label htmlFor={props.htmlForProp}>{props.textProp}</label>
+      <label htmlFor={props.htmlForProp} className={props.classNameLabelProp}>
+        {props.textProp}
+      </label>
       <input
         type={props.typeProp}
         min={props.minProp}
         step={props.stepProp}
         id={props.idProp}
-        className={props.clasNameProp}
+        className={props.classNameInputProp}
         value={props.valueProp}
+        // onChange={props.onChangeProp}
         required
       />
     </div>
   );
 }
 
+GrupoInput.propTypes = {
+  idProp: PropTypes.string.isRequired,
+  classNameLabelProp: PropTypes.string.isRequired,
+  typeProp: PropTypes.string.isRequired,
+  minProp: PropTypes.number,
+  stepProp: PropTypes.number,
+  valueProp: PropTypes.number.isRequired,
+//   onChangeProp: PropTypes.func.isRequired,
+};
 export default GrupoInput;

38bcec3dc6accd50cfc4d09e0b7790fbdace1348
Luis Federico Vilar Velazquez <luis_velazquez@estudante.sesisenai.org.br>
2023-03-17 15:40:16 -0300

    completado os 10 exercicios do modulo 1 semana 7...

--- src/scripts/MisFunctions.js
+++ src/scripts/MisFunctions.js
@@ -1,5 +1,5 @@
 export function calcularTotal(negociacoes) {
-  console.log(`Calculando o total de negociações : ${negociacoes} valor : ${negociacoes.valor} quantidade: ${negociacoes.quantidade}`);
+//   console.log(`Calculando o total de negociações : ${negociacoes} valor : ${negociacoes.valor} quantidade: ${negociacoes.quantidade}`);
   return negociacoes.reduce((total, n) => total + n.valorNegociacao * n.quantidadeNegociacao,0).toLocaleString("pt-BR", {
     style: "currency",
     currency: "BRL",
